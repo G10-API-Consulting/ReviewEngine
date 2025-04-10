46d0bc82198ee8ed750c85b8310f53611995f663
Ali Rezai <ali.rezai27461@gmail.com>
2025-04-10 15:19:12 +0200

    first commit

---
+++ src/main/java/com/example/ReviewEngine/ReviewEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReviewEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewEngineApplication.class, args);
+	}
+
+}
