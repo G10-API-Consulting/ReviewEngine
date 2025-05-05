e746a9a90f6144afbaa8979cbe284b6948574cc4
Ludvig Ekstam <l.ekstam@hotmail.com>
2025-05-05 14:22:48 +0200

    Created JwtAuthenticationFilter and added the filter to SecurityConfig. JwtAuthenticationFilter locks endpoints unless the user has a JWT token from using the /login endpoint.

--- src/main/java/com/example/ReviewEngine/config/SecurityConfig.java
+++ src/main/java/com/example/ReviewEngine/config/SecurityConfig.java
@@ -1,47 +1,41 @@
 package com.example.ReviewEngine.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
-            "/auth/register",  // Uppdatera denna rad från "/auth/register"
+            "/auth/register",
             "/auth/login",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui",
-            "/swagger-ui/",
-            "/webjars/**",
-            "/v3/api-docs/**",
+            // Swagger UI
             "/swagger-ui/**",
-            "/product",
-            "/product/save",
-            "/product/id",
-            "/user",
-            "/user/{id}",
-            "/user/all",
-            "/**"
+            "/v3/api-docs/**"
     };
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable());
         httpSecurity.authorizeHttpRequests(auth -> {
             auth.requestMatchers(AUTH_WHITELIST).permitAll();
             auth.anyRequest().authenticated();
         });
 
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
 }
+
 
 
 
