075eb5d8d82b09c3c8b2e897dfdd05f92e5a0dc0
Ali Rezai <ali.rezai27461@gmail.com>
2025-05-22 11:18:38 +0200

    databas

--- src/main/java/com/example/ReviewEngine/ReviewEngineApplication.java
+++ src/main/java/com/example/ReviewEngine/ReviewEngineApplication.java
@@ -1,4 +1,5 @@
 package com.example.ReviewEngine;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class ReviewEngineApplication {
 
 	public static void main(String[] args) {
+		if (!"prod".equalsIgnoreCase(System.getenv("SPRING_PROFILES_ACTIVE"))) {
+			Dotenv dotenv = Dotenv.configure()
+					.directory("C:/git/inlämning/grupparbete/ReviewEngine")
+					.ignoreIfMissing() // Ignorera om .env saknas
+					.load();
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		}
 		SpringApplication.run(ReviewEngineApplication.class, args);
 	}
 }
